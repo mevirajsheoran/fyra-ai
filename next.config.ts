a6fbe067807c53b26ec694d55289580ba0b28ed6
Viraj Sheoran <viraj.sheoran10@gmail.com>
2025-12-19 14:20:20 +0530

    chore: upgrade Next.js and fix security vulnerability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
